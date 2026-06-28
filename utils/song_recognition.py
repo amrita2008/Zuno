5e0f9efb956244a837b97416bd5b46627e893aff
Amrita <amritavaish16@gmail.com>
2026-06-29 01:57:45 +0530

    Update song_recognition.py

--- utils/song_recognition.py
+++ utils/song_recognition.py
@@ -1,12 +1,21 @@
-from pathlib import Path
-
 from shazamio import Shazam
 
 
-async def recognize_song(audio_path):
+async def recognize_song(audio_path: str):
 
     shazam = Shazam()
 
     result = await shazam.recognize(audio_path)
 
-    return result
+    if "track" not in result:
+        return None
+
+    track = result["track"]
+
+    return {
+        "title": track.get("title"),
+        "artist": track.get("subtitle"),
+        "album": track.get("sections", [{}])[0].get("metadata", []),
+        "cover": track.get("images", {}).get("coverart"),
+        "url": track.get("url"),
+    }
