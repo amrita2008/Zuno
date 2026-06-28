b8b79229fb8d0c27266cdcbcae9ecbb3a9cd5251
Amrita <amritavaish16@gmail.com>
2026-06-29 01:15:55 +0530

    Set up Streamlit app with home component

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
+import streamlit as st
+from components.home import show_home
 
+st.set_page_config(
+    page_title="Zuno",
+    page_icon="🎵",
+    layout="wide",
+    initial_sidebar_state="expanded"
+)
+
+show_home()
