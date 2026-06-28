cad78fa62977e11d1b36ffb09a41bafb398ccfed
Amrita <amritavaish16@gmail.com>
2026-06-29 01:45:56 +0530

    Add song recognition function using Shazam

--- utils/song_recognition.py
+++ utils/song_recognition.py
@@ -1,1 +1,12 @@
+from pathlib import Path
 
+from shazamio import Shazam
+
+
+async def recognize_song(audio_path):
+
+    shazam = Shazam()
+
+    result = await shazam.recognize(audio_path)
+
+    return result
